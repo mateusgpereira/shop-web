50534aaa1659bd7f303a19e891916b8b5ae47cc2
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2022-08-11 23:50:46 +0100

    creating base structure of components and organizing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { HomeComponent } from './home/home.component'
+import { FooterComponent } from './footer/footer.component'
+import { ProductListComponent } from './products/product-list/product-list.component'
+
+const materialModules = [MatToolbarModule]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    materialModules
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
